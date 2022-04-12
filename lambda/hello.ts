80900d44be45d46dd01c80f7880a79a06657a5e5
松井英俊 <dev.matsuihidetoshi@gmail.com>
2022-04-12 18:30:04 +0900

    [Add]event からパラメータ取得

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -1,9 +1,7 @@
 export const handler = async function (event: any, context: any) {
-  console.log(event)
-  console.log(context)
   return {
     statusCode: 200,
     headers: {},
-    body: { message: 'Hello from Lambda!' }
+    body: { message: "Hello from " + JSON.parse(event.body).hello }
   }
 }
