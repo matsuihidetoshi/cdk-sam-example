aabde713719b5c8d474ac463c7bfd9a70f7c3268
松井英俊 <dev.matsuihidetoshi@gmail.com>
2022-04-20 11:10:36 +0900

    [WIP]add env vars

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -1,7 +1,12 @@
 export const handler = async function (event: any, context: any) {
+  console.log("************ debug");
+  console.log(JSON.stringify(process.env));
   return {
     statusCode: 200,
     headers: {},
-    body: { message: "Hello from " + JSON.parse(event.body).hello }
-  }
-}
+    body: {
+      message:
+        "Hello from " + JSON.parse(event.body).hello + process.env["DB_HOST"],
+    },
+  };
+};
